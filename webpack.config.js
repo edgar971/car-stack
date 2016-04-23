557bcb6961730834ccf196d0481e2864789178cf
Edgar Pino <edgarbeltran@me.com>
2016-04-23 00:07:11 -0500

    added webpack support

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+
+
+module.exports = {
+    entry: [
+        'babel-polyfill',
+        './client.js'
+    ],
+    output: {
+        filename: 'public/js/bundle.js'
+    },
+    debug:true,
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|server.js)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+
+        ]
+    }
+}
+
