a59ae7ab12522005ddeb7b48c971cb10f9b93563
Edgar Pino <edgarbeltran@me.com>
2016-04-27 23:32:29 -0500

    added css and scss support on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
-var webpack = require('webpack');
-
+var webpack = require('webpack'),
+    ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    path = require('path');
 
 module.exports = {
     entry: [
-        './src/js/client.js'
+        './src/js/client.js',
+        './src/scss/app.scss'
     ],
     output: {
-        filename: 'public/js/bundle.js'
+        path: path.resolve('public/'),
+        filename: 'js/bundle.js'
     },
+    plugins: [
+        new ExtractTextPlugin("css/all.css")
+    ],
     debug:true,
     devtool: 'source-map',
     module: {
@@ -20,6 +26,12 @@ module.exports = {
                     presets: ['es2015']
                 }
             },
+            {
+                test: /\.scss$/,
+                exclude: /(node_modules)/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+
+            }
 
         ]
     }
