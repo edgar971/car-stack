1d822d0cf22e229cd3c27ef5a543cd1482d3a700
Edgar Pino <edgarbeltran@me.com>
2016-05-17 08:42:56 -0500

    added documentation for car entry model

--- db/models/EntryModel.js
+++ db/models/EntryModel.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     CarEntryModel;
-    
-    
+
+/**
+ * Car Entry Model
+ * @type {mongoose.Schema}
+ */
 CarEntryModel = new Schema({
     title: {
         type: String
